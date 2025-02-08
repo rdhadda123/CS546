64cf170ab9fe55a2fc89f9bcc8789ffd135dbb88
rdhadda123 <rdhadda@stevens.edu>
2025-02-08 01:32:57 -0500

    arrayUtils progress

--- Lab2/arrayUtils.js
+++ Lab2/arrayUtils.js
@@ -3,12 +3,87 @@
       DO NOT CHANGE THE FUNCTION NAMES
 */
 
-let arrayStats = (array) => {};
+export let arrayStats = (array) => {
+  if (!array) throw "Array does not exist"
+  if (!Array.isArray(array)) throw "Input is not an array"
+  if (array.length === 0) throw "Array has no elements"
+  array.forEach((value) => {
+    if (isNaN(value) || typeof value != "number")
+      throw `${value} is not a number`
+  })
 
-let mergeCommonElements = (...arrays) => {
-  //this function takes in a variable number of arrays that's what the ...arrays signifies
+  const sortedArray = array.sort()
+  const result = {mean: 0, median: 0, mode: 0, range: 0, minimum: 0, maximum: 0, count: 0, sum: 0}
+  const totalSum = 0.0
+  array.forEach((value) => {
+    totalSum += value
+  })
+
+  function getMedian(array) {
+    const mid = Math.floor(array.length / 2)
+    return array.length % 2 === 0 ? (array[mid - 1] + array[mid]) / 2 : array[mid]
+  }
+
+  function getMode(array) {
+    const mode = [], maxCounter = 1, counter = 1
+
+    for (const i = 1; i < array.length; i += 1){
+      if (array[i] === array[i - 1])
+        counter++
+      else
+        counter = 1
+
+      if (counter > maxCounter){
+        maxCounter = counter
+        mode = array[i]
+      } else if (counter == maxCounter) {
+        mode.push(array[i])
+      }
+    }
+
+    if (mode.length === 0)
+      return 0
+    else if (mode.length === 1)
+      return mode[0]
+    else
+      return mode
+  }
+
+  result.mean = totalSum / sortedArray.length
+  result.median = getMedian(sortedArray)
+  result.mode = getMode(sortedArray)
+  result.range = sortedArray[sortedArray.length - 1] - sortedArray[0]
+  result.minimum = sortedArray[0]
+  result.maximum = sortedArray[sortedArray.length - 1]
+  result.count = sortedArray.length
+  result.sum = totalSum
+
+  return result
 };
 
-let numberOfOccurrences = (...arrays) => {
+export let mergeCommonElements = (...arrays) => {
+  //this function takes in a variable number of arrays that's what the ...arrays signifies
+  if (arrays.length < 2) throw "Atleast two arrays need to be inputted"
+  arrays.forEach((array) => {
+    if (!Array.isArray(array))
+      throw `${array} is not an array`
+    if (array.length === 0)
+      throw `${array} needs to have atleast one element`
+    array.forEach((value) => {
+      if (!((typeof value === "number") || (typeof value === "string") || Array.isArray(value)))
+        throw `Each ${array} element needs to be a number, string, or array`
+      if (Array.isArray(value)){
+        const flattened = value.flat(Infinity)
+        flattened.forEach((item) => {
+          if (!((typeof item === "number") || (typeof item === "string")))
+            throw `Each ${flattened} element needs to be a number or string`
+        })
+      }
+    })
+  })
+
+};
+
+export let numberOfOccurrences = (...arrays) => {
   //this function takes in a variable number of arrays that's what the ...arrays signifies
 };
