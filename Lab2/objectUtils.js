5940beb3c267966935960aa26c880c74f0dc6722
rdhadda123 <rdhadda@stevens.edu>
2025-02-11 11:42:00 -0500

    Refactor commonKeysValues

--- Lab2/objectUtils.js
+++ Lab2/objectUtils.js
@@ -39,32 +39,24 @@ export let commonKeysValues = (obj1, obj2) => {
       const keys1 = Object.keys(obj1);
       const keys2 = Object.keys(obj2);
       
-      keys1.forEach((key) => {
+      for (let key of keys1) {
             if (keys2.includes(key)) {
-                  const val1 = obj1[key];
-                  const val2 = obj2[key];
-
-                  if (val1 === val2) 
+                const val1 = obj1[key];
+                const val2 = obj2[key];
+    
+                if (val1 === val2) {
+                    result[key] = val1;
+                } else if (typeof val1 === "object" && typeof val2 === "object") {
+                    const nestedKeys1 = Object.keys(val1);
+                    const nestedKeys2 = Object.keys(val2);
+    
+                    if (nestedKeys1.every(nestedKey => nestedKeys2.includes(nestedKey) && val1[nestedKey] === val2[nestedKey])) {
                         result[key] = val1;
-                  else if (typeof val1 === 'object' && typeof val2 === 'object') {
-                        let nested = true;
-                        const nestedKeys1 = Object.keys(val1);
-                        const nestedKeys2 = Object.keys(val2);
-
-                        nestedKeys1.forEach((nestedKey) => {
-                        if (nestedKeys2.includes(nestedKey)) {
-                              if (val1[nestedKey] !== val2[nestedKey]) 
-                                    nested = false;
-                              else 
-                                    result[nestedKey] = val1[nestedKey];
-                        } else 
-                              nested = false;
-                        })
-                        if (nested) 
-                              result[key] = val1;
-                  }
+                        nestedKeys1.forEach(nestedKey => result[nestedKey] = val1[nestedKey]);
+                    }
+                }
             }
-      })
+      }
 
       return result;
 };
@@ -77,11 +69,11 @@ export let calculateObject = (object, func) => {
             throw `${object} needs to be an object`
       if (typeof func !== "function")
             throw `${func} needs to be a function`
-      for (const [key, value] of Object.entries(object)) {
+      for (let [key, value] of Object.entries(object)) {
             if (typeof value !== "number") {
                 throw `Value of "${key}" must be a valid number`;
             }
-            const res = func(value)
+            let res = func(value)
             resultObj[key] = Math.sqrt(res).toFixed(2)
       }
 
