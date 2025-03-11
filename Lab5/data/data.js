572115fff5f9380a1f988846bdffd1e0a8a91828
rdhadda123 <rdhadda@stevens.edu>
2025-03-11 10:09:41 -0400

    Implement people route

--- Lab5/data/data.js
+++ Lab5/data/data.js
@@ -4,49 +4,33 @@ to get the stocks, people, getStockById, getPersonById.  You will import these f
 import {getPeopleData, getStocksData, checkId} from '../helpers.js'
 
 export const getStocks = async () => {
-    try {
-        const stocksCollection = await getStocksData()
-        return stocksCollection.data
-    } catch (e) {
-        throw "Failed to fetch to stocks data"
-    }
+    const stocksCollection = await getStocksData()
+    return stocksCollection.data
 };
 
 export const getPeople = async () => {
-    try {
-        const peopleCollection = await getPeopleData()
-        return peopleCollection.data
-    } catch (e) {
-        throw "Failed to fetch to people data"
-    }
+    const peopleCollection = await getPeopleData()
+    return peopleCollection.data
 };
 
 export const getStockById = async (id) => {
     id = checkId(id)
-    try {
-        const stocksCollection = await getStocksData()
-        for (const stock of stocksCollection) {
-            if (stock.id === id)
-                return stock
-        }
-
-        throw `Stock with id: ${id} not found`
-    } catch (e) {
-        throw e
+    const stocksCollection = await getStocksData()
+    for (const stock of stocksCollection.data) {
+        if (stock.id === id)
+            return stock
     }
+
+    throw `Stock with id: ${id} not found`
+    
 };
 
 export const getPersonById = async (id) => {
     id = checkId(id)
-    try {
-        const peopleCollection = await getPeopleData()
-        for (const person of peopleCollection) {
-            if (person.id === id)
-                return person
-        }
-
-        throw `Person with id: ${id} not found`
-    } catch (e) {
-        throw e
+    const peopleCollection = await getPeopleData()
+    for (const person of peopleCollection.data) {
+        if (person.id === id)
+            return person
     }
+    throw `Person with id: ${id} not found`
 };
--- Lab5/routes/people.js
+++ Lab5/routes/people.js
@@ -2,12 +2,39 @@
 //Lecture Code Refernece -> https://github.com/stevens-cs546-cs554/CS-546/tree/master/lecture_05/routes
 
 //You can import your getPeople() function in the /data/data.js file to return the list of people.  You can also import your getPersonById(id) function and call it in the :/id route.
+import express from 'express'
+import { getPeople, getPersonById } from '../data/data.js';
+import { checkId } from '../helpers.js';
+
 const router = express.Router();
 
-router.route('/');
+router
+    .route('/')
+    .get(async (req, res) => {
+        try {
+            const peopleList = await getPeople()
+            return res.json(peopleList)
+        } catch (e) {
+            return res.status(500).send(e)
+        }
+    })
 // Implement GET Request Method and send a JSON response  See lecture code!
 
-router.route('/:id');
+router
+    .route('/:id')
+    .get(async (req, res) => {
+        try {
+            req.params.id = checkId(req.params.id)
+        } catch (e) {
+            return res.status(400).json({error: e})
+        }
+        try {
+            const person = await getPersonById(req.params.id)
+            return res.json(person)
+        } catch (e) {
+            return res.status(404).json(e)
+        }
+    })
 // Implement GET Request Method and send a JSON response See lecture code!
 
 export default router;
