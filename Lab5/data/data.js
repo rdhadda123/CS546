ee8c4836ee99fb012a5285e0909135a93344832b
rdhadda123 <rdhadda@stevens.edu>
2025-03-11 13:17:48 -0400

    Finish Lab5

--- Lab5/data/data.js
+++ Lab5/data/data.js
@@ -5,18 +5,18 @@ import {getPeopleData, getStocksData, checkId} from '../helpers.js'
 
 export const getStocks = async () => {
     const stocksCollection = await getStocksData()
-    return stocksCollection.data
+    return stocksCollection
 };
 
 export const getPeople = async () => {
     const peopleCollection = await getPeopleData()
-    return peopleCollection.data
+    return peopleCollection
 };
 
 export const getStockById = async (id) => {
     id = checkId(id)
     const stocksCollection = await getStocksData()
-    for (const stock of stocksCollection.data) {
+    for (const stock of stocksCollection) {
         if (stock.id === id)
             return stock
     }
@@ -28,7 +28,7 @@ export const getStockById = async (id) => {
 export const getPersonById = async (id) => {
     id = checkId(id)
     const peopleCollection = await getPeopleData()
-    for (const person of peopleCollection.data) {
+    for (const person of peopleCollection) {
         if (person.id === id)
             return person
     }
