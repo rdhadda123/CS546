9f6c653644512d1e19c99c15989dd0acb990f6a7
rdhadda123 <rdhadda@stevens.edu>
2025-03-25 23:55:18 -0400

    Finish setting up app

--- Lab6/app.js
+++ Lab6/app.js
@@ -1,1 +1,11 @@
 //here is where you'll set up your server as shown in lecture code.
+import express from 'express'
+const app = express()
+import configRoutesFunction from './routes/index.js'
+
+configRoutesFunction(app)
+
+app.listen(3000, () => {
+    console.log("We've now got a server!");
+    console.log('Your routes will be running on http://localhost:3000');
+})
