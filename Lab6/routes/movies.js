a37ce9595e83c581c15f42c34b48519b5361cc01
rdhadda123 <rdhadda@stevens.edu>
2025-03-25 21:07:13 -0400

    Finish movie post '/' route

--- Lab6/routes/movies.js
+++ Lab6/routes/movies.js
@@ -1,6 +1,7 @@
 //require express and express router as shown in lecture code
 import express from 'express'
 import { movieData } from '../data/index.js';
+import { checkCastMembers, checkDateReleased, checkDirector, checkGenres, checkID, checkRating, checkRuntime, checkString, checkStudio, checkTitle } from '../helpers';
 
 const router = express.Router()
 router
@@ -16,6 +17,44 @@ router
   })
   .post(async (req, res) => {
     //code here for POST
+    let movieInfo = req.body;
+    if (!movieInfo || Object.keys(movieInfo).length === 0) {
+      return res
+        .status(400)
+        .json({error: 'There are no fields in the request body'})
+    }
+
+    try {
+      movieInfo._id = checkID(movieInfo._id)
+      movieInfo.title = checkTitle(movieInfo.title)
+      movieInfo.plot = checkString(movieInfo.plot)
+      movieInfo.genres = checkGenres(movieInfo.genres)
+      movieInfo.rating = checkRating(movieInfo.rating)
+      movieInfo.studio = checkStudio(movieInfo.studio)
+      movieInfo.director = checkDirector(movieInfo.director)
+      movieInfo.castMembers = checkCastMembers(movieInfo.castMembers)
+      movieInfo.dateReleased = checkDateReleased(movieInfo.dateReleased)
+      movieInfo.runtime = checkRuntime(movieInfo.runtime)
+    } catch (e) {
+      return res.status(400).json({error: e})
+    }
+
+    try {
+      let newMovie = await movieData.createMovie(
+        movieInfo.title,
+        movieInfo.plot,
+        movieInfo.genres,
+        movieInfo.rating,
+        movieInfo.studio,
+        movieInfo.director,
+        movieInfo.castMembers,
+        movieInfo.dateReleased,
+        movieInfo.runtime
+      )
+      return res.json(newMovie)
+    } catch (e) {
+      return res.sendStatus(500)
+    }
   });
 
 router
