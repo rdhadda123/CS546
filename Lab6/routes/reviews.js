a8647e68c590d92bef5fae0d939935be9b938f5b
rdhadda123 <rdhadda@stevens.edu>
2025-03-25 23:04:54 -0400

    Finish review get '/:movieId' route

--- Lab6/routes/reviews.js
+++ Lab6/routes/reviews.js
@@ -1,9 +1,31 @@
 //require express and express router as shown in lecture code
+import express from 'express'
+import { reviewData } from '../data/index.js';
+import { movieData } from '../data/index.js';
+import { checkID } from "../helpers";
 
 router
   .route('/:movieId')
   .get(async (req, res) => {
     //code here for GET
+    try {
+      req.params.movieId = checkID(movieId)
+    } catch (e) {
+      return res.status(400).json({error: e})
+    }
+    
+    try {
+      await movieData.getMovieById(req.params.movieId)
+    } catch (e) {
+      return res.status(404).send(e)
+    }
+
+    try {
+      let review = await reviewData.getAllReviews(req.params.movieId)
+      return res.json(review)
+    } catch (e) {
+      return res.status(404).send(e)
+    }
   })
   .post(async (req, res) => {
     //code here for POST
