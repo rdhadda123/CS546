0201c4b814011988413f6810d789ff8ae00e03c9
rdhadda123 <rdhadda@stevens.edu>
2025-04-14 21:17:32 -0400

    Finish index.js route

--- Lab9/routes/index.js
+++ Lab9/routes/index.js
@@ -1,1 +1,12 @@
 //Here you will import route files and export them as used in previous labs.
+import arrayRoutes from './arraySort.js'
+
+const constructorMethod = (app) => {
+    app.use('/', arrayRoutes);
+    app.use('*', (req, res) => {
+        //Do I have to redirect to '/' page?
+        return res.status(404).json({error: 'Not found'});
+    });
+  };
+  
+export default constructorMethod;
