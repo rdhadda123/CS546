7593b8ae5eeb356d7b9250120f37433882fde74a
rdhadda123 <rdhadda@stevens.edu>
2025-04-25 20:34:39 -0400

    Finish middleware and app.js

--- Lab10/app.js
+++ Lab10/app.js
@@ -52,3 +52,95 @@ Then call next
 
 
 */
+import express from 'express'
+const app = express()
+import session from 'express-session'
+import configRoutes from './routes/index.js'
+import exphbs from 'express-handlebars'
+
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+app.use(
+     session({
+          name: 'AuthenticationState',
+          secret: "This is a secret.. shhh don't tell anyone",
+          saveUninitialized: false,
+          resave: false
+     })
+)
+
+app.use((req, res, next) => {
+     const date = new Date().toUTCString()
+     const method = req.method
+     const path = req.path
+     const authenticated = req.session.user
+     const status = '(Non-Authenticated)'
+     if (authenticated){
+          if (req.session.user.role === 'superuser')
+               status = '(Authenticated Super User)'
+          else {
+               status = '(Authenticated User)'
+          }
+     }
+     console.log(`[${date}]: ${method} ${path} ${status}`)
+     next()
+})
+
+app.use('/login', (req, res, next) => {
+     if (req.session.user){
+          if (req.session.user === 'superuser'){
+               return res.redirect('/superuser')
+          } else {
+               return res.redirect('/user')
+          }
+     }
+     next()
+})
+
+app.use('/register',(req, res, next) => {
+     if (req.session.user){
+          if (req.session.user === 'superuser'){
+               return res.redirect('/superuser')
+          } else {
+               return res.redirect('/user')
+          }
+     }
+     next()
+})
+
+app.use('/user',(req, res, next) => {
+     if (!req.session.user)
+          return res.redirect('/login')
+     next()
+})
+
+app.use('/superuser',(req, res, next) => {
+     if (!req.session.user)
+          return res.redirect('/login')
+     if (req.session.user.role !== 'superuser'){
+          return res.status(403).render('error', {
+               error: "Access denied. You are not a superuser",
+               userLink: '/user'
+          })
+     }
+
+     next()
+})
+
+app.use('/signout', (req, res, next) => {
+     if (!req.session.user)
+          return res.redirect('/login')
+     next()
+})
+
+
+app.engine('handlebars', exphbs.engine({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars')
+
+configRoutes(app)
+
+app.listen(3000, () => {
+     console.log("We've now got a server!");
+     console.log('Your routes will be running on http://localhost:3000');
+})
